3cea8cbd13af03407ce069f8459eb47340277bc4
Juan David Loaiza Reyes <joumanmaster_@outlook.es>
2022-10-12 17:18:51 -0500

    ready hole TODO

--- src/controller.py
+++ src/controller.py
@@ -9,13 +9,13 @@ def main():
         validator = True
         while validator:
             try:
-                selector = input("Para entrar a 'materia' presione 1, para entrar en 'historia academica' presione 2, para entrar en 'estudiante' presione 3, para salir presione 4.")
+                selector = input("Para entrar a 'materia' presione 1, para entrar en 'historia academica' presione 2, para entrar en 'estudiante' presione 3, para salir presione 4. ")
                 selector = int(selector)
                 validator = False
                 while (selector!=1 and selector!=2 and selector!=3 and selector !=4):
-                    selector = input(f"{selector} no es valido")
+                    selector = input(f"{selector} no es valido ")
                     selector = int(selector)
-            except:
+            except ValueError:
                 print("input invalido")
                 validator = True
         if selector==1:
