69c8b8731d5ca9f4447824ba96f69a74e313527c
Juan David Loaiza Reyes <joumanmaster_@outlook.es>
2022-10-26 12:13:01 -0500

    doc full

---
+++ docs/console_utils.py
@@ -0,0 +1,45 @@
+import os
+
+#Modulo para la creación de tablas en consola y ordenar la informacion
+from prettytable import PrettyTable
+
+
+#Funcion para limpiar la consola
+def clearConsole():
+    command = 'clear' #Para sistemas linux
+    if os.name in ('nt', 'dos'):  # En caso de que este corriendo sobre windows
+        command = 'cls' #Sistemas windows
+    os.system(command) #Ejecución comando
+
+#Tabla para materias
+def tableMaterias(data):
+    my_table = PrettyTable() #llamado al modulo de tablas en consola
+    my_table.field_names = ["CODIGO", "NOMBRE", "FACULTAD", "DEPARTAMENTO", "IDIOMA", "CREDITOS"] #Nombre de los campos
+    for i in range(len(data)):#Ciclo para añadir los datos
+        my_table.add_row(data[i])
+    print(my_table)#Impresion de los datos
+
+#Tabla de historia academica
+def tableHistoriaAcad(data):
+    my_table = PrettyTable() #llamado al modulo de tablas en consola
+    my_table.field_names = ["ID", "CODIGO MATERIA", "ID ESTUDIANTE", "NOTA", "CREDITOS"] #Nombre de los campos
+    for i in range(len(data)):#Ciclo para añadir los datos
+        my_table.add_row(data[i])
+    print(my_table)#Impresion de los datos
+
+#Tabla Estudiante
+def tableEstudiante(data):
+    my_table = PrettyTable() #llamado al modulo de tablas en consola
+    my_table.field_names = ["IDENTIFICACION", "NOMBRE", "APELLIDO", "CARRERA", "FECHA NACIMIENTO", "FECHA INGRESO", "PROCEDENCIA", "CORREO ELECTRONICO", "CANTIDAD DE MATRICULAS"] #Nombre de los campos
+    for i in range(len(data)):#Ciclo para añadir los datos
+        my_table.add_row(data[i])
+    print(my_table)#Impresion de los datos
+
+
+#Tabla de historia academica
+def tableRanking(data):
+    my_table = PrettyTable() #llamado al modulo de tablas en consola
+    my_table.field_names = ["ID", "PROMEDIO"] #Nombre de los campos
+    for i in range(len(data)):#Ciclo para añadir los datos
+        my_table.add_row(data[i])
+    print(my_table)#Impresion de los datos
---
+++ docs/ranking.doc.py
@@ -0,0 +1,36 @@
+from types import NoneType
+from estudiante import readAllRows
+from historia_academica import prom, acadHistoryById
+from console_utils import tableRanking
+
+#Busca todos los ids de la tabla estudiante y los guarda en una lista y devuelve esta lsita
+def idsList():
+    ids =[]
+    data = readAllRows() #Data de todos los estudiantes
+    for x in range(len(data)):
+        ids.append(data[x][0]) #Acomoda los ids en lista
+    return ids
+
+#Devuelve el promedio junto a quien le pertenece
+def promsCalculator(data):
+    proms = []
+    for i in range(len(data)):
+        datos = acadHistoryById(data[i])
+        promedy =prom(datos)
+        if type(promedy)==NoneType:
+            promedy = 0.0
+        proms.append({data[i]:promedy}) #Organiza los datos como un diccionario y almacena este diccionario en una lista
+    return proms #Devuelve una lista de diccionarios con las notas y sus respectivos dueños
+
+#Convierte la lista de dictionarios en una lista de tuplas ordenadas
+def dictToTuple(data):
+    result = []
+    for i in range(len(data)):
+        result.append(data[i].popitem())
+    return result
+
+def main():
+    data = idsList()#buscador de los ids
+    promsList = promsCalculator(data)#Generación de los datos
+    dataToOrder=dictToTuple(promsList)#Guardado de tupla con los datos de la lista
+    tableRanking(sorted(dataToOrder, key=lambda x: x[1], reverse=True)) #Ordena e imprime la lista de tuplas en orden descendente de acuerdo a la nota que estara siempre en indes = 1
