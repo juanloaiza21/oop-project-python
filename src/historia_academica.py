8ad4f793103cf53c87e8a1517b754c49e84a4220
Nicolas Rozo <114628919+Nicolukas2820@users.noreply.github.com>
2022-10-26 09:34:30 -0500

    Update historia_academica.py

--- src/historia_academica.py
+++ src/historia_academica.py
@@ -11,8 +11,8 @@ DB = config('DB_NAME')
 #Crea tablas manualmente, automatizar; TODO generar modulo que cree toda la DB y meterlo en el controlador como primer paso al correr la app
 def createTeable():
     try:
-        conn = sql.connect(DB)
-        cursor = conn.cursor()
+        conn = sql.connect(DB) #Conexion a la base de datos
+        cursor = conn.cursor()  #Se inserta un cursor para la insercion de datos
         cursor.execute(
             """CREATE TABLE IF NOT EXISTS acadhistory (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -22,57 +22,57 @@ def createTeable():
                 creditos_cursados INTEGER,
                 FOREIGN KEY(id_estudiante) REFERENCES estudiante(identificacion),
                 FOREIGN KEY(code_materia) REFERENCES materias(codigo)
-                )"""
+                )"""   #Creacion de la tabla
         );
-        conn.commit();
-        conn.close();
-    except sql.Error as e:
-        print(e)
+        conn.commit();#Se verifican los cambios en la base de datos
+        conn.close(); # se cierra la conexion con la base de datos
+    except sql.Error as e: #Sentencia que se ejecuta en caso de error
+        print(e) #Mensaje presentado en caso de error
 
 #----------------------------------------------------------------Inserta las notas de la materia-------------------------------------------------------------------
 
 def creditsRef(idMateria: int):
     try:
         data = materiasFilter('codigo', idMateria)
-        return data[0][5]
-    except:
-        print("Codigo de la materia inexistente")
+        return data[0][5] #Retorna el codigo de la materia junto a los creditos
+    except:  
+        print("Codigo de la materia inexistente") #En caso de no existir materia se ejecuta la siguiente linea
 
 #Crear historia academica en documentación
 def insertRow(codigo: int, id_estudiante: int, score: float):
     try:
-        conn = sql.connect(DB)
+        conn = sql.connect(DB) #Conexion a la base de datos
         conn.execute('PRAGMA foreign_keys = ON') #Valida las llaves extranjeras, es decir la existencia del estudiante o la amteria, si no existe da error
         cursor = conn.cursor()
-        creditos= creditsRef(codigo)
-        instruction = f"INSERT INTO acadhistory(code_materia, id_estudiante, nota, creditos_cursados) VALUES ({codigo},{id_estudiante}, {score}, {creditos})"
-        cursor.execute(instruction)
+        creditos= creditsRef(codigo) #llamado a la funcion de filtro de materias
+        instruction = f"INSERT INTO acadhistory(code_materia, id_estudiante, nota, creditos_cursados) VALUES ({codigo},{id_estudiante}, {score}, {creditos})" #Da la instruccion de insertar datos en la base de datos
+        cursor.execute(instruction)#Ejecucion de la instruccion
         conn.commit();
-        conn.close();
-        print("Datos añadidos: ")
+        conn.close(); #Cierre de la conexion a la base de datos
+        print("Datos añadidos: ") #Muestra en pantalla los datos añadidos
         tableHistoriaAcad([(0,codigo, id_estudiante, score, creditos)])
     except sql.Error as e:
-        print (e)
+        print (e)  #En caso de error se debe verificar la existencia de los datos
         print('Probablemente no exista el estudiante o la materia, revise por favor los datos que ingreso y vuelva a intentar. Si sí existen, es que primero debe eliminar la materia, para que se actualice con los nuevos datos')
 
 #Lector del input por parte del usuario
 def rowGetter():
     while True:
         try:
-            print("El codigo de la materia y el Id del estudiante deben existir, si no, habra error")
-            codigo = input('Codigo de la materia: ')
-            codigo = codigo.ljust(10)
-            idEstudiante = input('Id del estudiante ')
-            selector = int(input("¿Desea agregar nota? 1. Si. 2. No"))
-            if selector ==1:
+            print("El codigo de la materia y el Id del estudiante deben existir, si no, habra error") #Aclaracion para evitar posibles errores en el programa
+            codigo = input('Codigo de la materia: ') #El usuario ingresa el codigo
+            codigo = codigo.ljust(10) #El codigo no puede tener mas de 10 caracteres
+            idEstudiante = input('Id del estudiante ') #Se ingresa el Id del estudiante
+            selector = int(input("¿Desea agregar nota? 1. Si. 2. No")) #El usuario decide entre insertar o no insertar nota
+            if selector ==1: #Si el usuario selecciona 1 podra insertar la nota
                 nota = input('Nota del estudiante ')
-            else:
+            else: #en caso de no insertar nota aparecera como 0.0
                 nota = 0.0
-            nota = float(nota)
-            idEstudiante = int(idEstudiante)
-            codigo = int(codigo)
-            return (codigo, idEstudiante, nota)
-        except ValueError:
+            nota = float(nota) #se transforma la nota en un dato tipo float
+            idEstudiante = int(idEstudiante) #se transforma el Id del estudiante en un dato tipo int
+            codigo = int(codigo) #se transforma el codigo en un dato tipo int
+            return (codigo, idEstudiante, nota) #retorna codigo, id y la nota
+        except ValueError: #En caso de error dira que los datos son invalidos
             print("Dato(s) invalido")
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------#
@@ -82,14 +82,14 @@ def rowGetter():
 #Esta función debe traer todos las notas de un id
 def acadHistoryById(id: int):
     try:
-        conn = sql.connect(DB)
+        conn = sql.connect(DB) #Conexion a la base de datos
         cursor = conn.cursor()
-        instruction = f"SELECT * from acadhistory WHERE id_estudiante={id} ORDER BY nota DESC"
-        cursor.execute(instruction)
+        instruction = f"SELECT * from acadhistory WHERE id_estudiante={id} ORDER BY nota DESC" #da la instruccion de buscar las notas en la base de datos
+        cursor.execute(instruction) #Ejecuta la instruccion de buscar las notas
         datos = cursor.fetchall()
-        conn.commit();
+        conn.commit(); 
         conn.close()
-        return datos;
+        return datos; #retorna los datos obtenidos con el fetchall
     except sql.Error as e:
         print (e)
 
@@ -98,152 +98,153 @@ def prom (data):
     try:
         numeroMaterias = len(data)
         sumaNotas = 0
-        for i in range(numeroMaterias):
+        for i in range(numeroMaterias): #toma todas las notas
             sumaNotas += data[i][3]
-        return sumaNotas/numeroMaterias
+        return sumaNotas/numeroMaterias #saca el promedio de las notas basado en la cantidad de materias
     except:
-        print("Try again")
+        print("Try again") #En caso de error imprimira "Intenta otra vez"
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
 #----------------------------------------------------------------Metodo de actualizacion--------------------------------------------------------------------------#
 def update(fieldOnChange: str, dataOnChange, code: int, dataOnChange2: int):
     try:
-        conn = sql.connect(DB)
+        conn = sql.connect(DB) #conexion a la base de datos
         cursor = conn.cursor()
         try:
-            dataOnChange= float(dataOnChange)
-            instruction = f"UPDATE acadhistory SET '{fieldOnChange}'={dataOnChange} WHERE code_materia={dataOnChange2} AND id_estudiante={code}"
+            dataOnChange= float(dataOnChange) #convierte la variable en un dato de tipo float
+            instruction = f"UPDATE acadhistory SET '{fieldOnChange}'={dataOnChange} WHERE code_materia={dataOnChange2} AND id_estudiante={code}" #Da la instruccion de actualizar
         except ValueError:
             instruction = f"UPDATE acadhistory SET '{fieldOnChange}'='{dataOnChange}' WHERE code_materia={code} AND id_estudiante={dataOnChange2}" #Comando de actualización en SQL
         finally:
-            cursor.execute(instruction)
+            cursor.execute(instruction) #Ejecuta la instruccion de actualizacion
             conn.commit();
-            conn.close()
-            print ('Datos actualizados de forma correcta')
+            conn.close() #se cierra la conexxion
+            print ('Datos actualizados de forma correcta') #Informa la correcta actualizacion de datos
     except sql.Error as e:
         print (e)
 
 def rowUpdateGetter():
     while True:
         try:
-            print("El codigo de la materia y el Id del estudiante deben existir, si no, habra error")
-            codigo = input('Codigo de la materia: ')
-            codigo = codigo.ljust(10)
-            idEstudiante = input('Id del estudiante ')
-            nota = input('Nota del estudiante ')
-            return (int(codigo), int(idEstudiante), float(nota))
+            print("El codigo de la materia y el Id del estudiante deben existir, si no, habra error")#Aclaracion para evitar que el programa tenga errores
+            codigo = input('Codigo de la materia: ')#El usuario ingresa el codigo
+            codigo = codigo.ljust(10)#El codigo no puede tener mas de 10 caracteres
+            idEstudiante = input('Id del estudiante ')#El usuario ingresa el id
+            nota = input('Nota del estudiante ')#El usuario ingresa la nota
+            return (int(codigo), int(idEstudiante), float(nota)) #retorna los datos insertados como datos tipo int y float
         except ValueError:
-            print("Dato(s) invalido")
+            print("Dato(s) invalido") #En caso de que los datos sean incorrectos saldra este mensaje
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
 #----------------------------------------------------------------Metodo de borrado--------------------------------------------------------------------------------#
 def deleteRow(code: int, studentId: int):
     try:
-        conn = sql.connect(DB)
+        conn = sql.connect(DB) #Conexxion a la base de datos
         cursor = conn.cursor()
         instruction = f"DELETE FROM acadhistory WHERE code_materia={code} AND id_estudiante={studentId}" #Comando de delete en SQL un solo dato
-        cursor.execute(instruction)
+        cursor.execute(instruction) #Ejecuta la instruccion de borrado
         conn.commit();
-        conn.close()
-        print ('Datos actualizados de forma correcta')
+        conn.close() #Cierre de conexion a la base de datos
+        print ('Datos actualizados de forma correcta') #Mensaje en caso de no hbaer error en la actualizacion
     except sql.Error as e:
         print (e)
 
 def rowDeleteGetter():
     try:
-        print("El codigo de la materia y el Id del estudiante deben existir, si no, habra error")
-        codigo = input('Codigo de la materia: ')
-        codigo = codigo.ljust(10)
-        idEstudiante = input('Id del estudiante ')
-        return (int(codigo), int(idEstudiante))
+        print("El codigo de la materia y el Id del estudiante deben existir, si no, habra error")#Aclaracion para prevenir errores en el programa
+        codigo = input('Codigo de la materia: ') #El usuario ingresa el codigo
+        codigo = codigo.ljust(10) #El codigo ingresado no puede tener mas de 10 caracteres
+        idEstudiante = input('Id del estudiante ') #El usuario ingresa el id
+        return (int(codigo), int(idEstudiante)) #Retorna codigo e id
     except ValueError:
-        print("Dato(s) invalido")
+        print("Dato(s) invalido") #En caso de insertar datos incorrectos se imprime este mensaje
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
 #----------------------------------------------------------------Controlador principal----------------------------------------------------------------------------#
 def main():
-    #TODO print historia academica como una tabla xd
+    #TODO print historia academica como una tabla 
     while True: 
         validator = True
         while validator:
             try:
                 selector=input("Si desea crear añadir una materia aprete 1. Si desea actualizar una nota en una materia ya registrada aprete 2. Si desea consultar sus notas o su promedio aprete 3. si desea eliminar alguna materia aprete 4. Para salir, cualquier otra tecla.")
-                selector =int(selector)
+                #El usuario decide que desea realizar en el programa
+                selector =int(selector) #trensforma la seleccion del usuario para poder hacer una validacion
                 if (selector!=1 and selector !=2 and selector !=3 and selector !=4 and selector !=5):
-                    print(f"{selector} es un valor invalido")
+                    print(f"{selector} es un valor invalido")#En caso de que el usuario seleccione una opcion que no sea parte de las expuestas por el programa saldra este mensaje de error
                 else:
                     validator = False
             except ValueError:
                 print(f"{selector} es un valor invalido")
-        if selector == 1:
+        if selector == 1: #ejecucion en caso de que la seleccion del usuario sea 1
             validator = True
             while validator:
                 # TODO Se arma bucle infinito en 1
                 try:
                     clearConsole()
-                    selectorr=input("Para añadir una materia debe escribir el documento del estudiante (ya registrado) y  el codigo de una materia existente. ¿Desea ver que materias hay disponibles? 1.Si. 2.No");
+                    selectorr=input("Para añadir una materia debe escribir el documento del estudiante (ya registrado) y  el codigo de una materia existente. ¿Desea ver que materias hay disponibles? 1.Si. 2.No"); #da la opcion de ver las materias existentes en la base de datos
                     selectorr = selectorr
-                    if selectorr == "1":
+                    if selectorr == "1": #En caso de querer ver las materias se ejecuta las siguientes lineas
                         allMaterias('codigo')
                         sleep(2.5)
                         print('A continuación va a inscribir una materia ')
                         data = rowGetter()
-                        insertRow(data[0], data[1], data[2])
+                        insertRow(data[0], data[1], data[2]) 
                         validator = False
-                    elif selectorr == "2":
+                    elif selectorr == "2": #En caso de no querer ver las materias se ejecutan las siguientes lineas
                         print('A continuación va a inscribir una materia ')
                         data = rowGetter()
                         insertRow(data[0], data[1], data[2])
                         validator = False
                 except ValueError:
-                    print (f"{selector} es invalido")
-        elif selector == 2:
+                    print (f"{selector} es invalido") #Error en caso de no insertar una opcion disponible
+        elif selector == 2: #ejecucion en caso de que la seleccion del usuario sea 2
             while True:
                 try:
                     clearConsole()
                     print("Para actualizar la nota de una materia debe escribir el documento del estudiante y el codigo de la materia. Claramene la nota tambien");
-                    data = rowUpdateGetter()
+                    data = rowUpdateGetter() #se ejecuta la funcion de update
                     update('nota', data[2], data[1], data[0])
                     break
                 except:
-                    print ("Unexpected error")
-        elif selector == 3: 
+                    print ("Unexpected error") #mensaje en caso de error
+        elif selector == 3: #ejecucion en caso de que la seleccion del usuario sea 3
             while True:
                 try:
                     clearConsole()
                     selector=input("Si desea ver su promedio aprete 1. Si desea ver todas sus notas aprete 2.");
                     selector = int(selector)
                     if (selector !=1 and selector !=2):
-                        print(f"{selector} es invalido")
-                    elif selector == 1:
+                        print(f"{selector} es invalido") #Validacion de la opcion insertada
+                    elif selector == 1: #Ejecucion en caso de la opcion 1
                         while True:
                             try:
                                 idd = int(input("Ingrese el documento del estudiante "))
-                                data = acadHistoryById(idd)
-                                promedio = prom(data)
+                                data = acadHistoryById(idd) #se ejecuta la funcion de historia academica
+                                promedio = prom(data) #Saca el promedio de las notas
                                 print("Su promedio es: ", promedio)
                                 break
                             except ValueError:
                                 print(f"{idd} es invalido")
-                    elif selector == 2:
+                    elif selector == 2: #Ejecucion en caso de la opcion 2
                        while True:
                             try:
                                 clearConsole()
-                                idd = int(input("Ingrese el documento del estudiante "))
+                                idd = int(input("Ingrese el documento del estudiante ")) #solicita el id de estudiante 
                                 data = acadHistoryById(idd)
-                                print("Sus notas son: ")
-                                tableHistoriaAcad(data)
+                                print("Sus notas son: ") #muestra las notas del estudiante
+                                tableHistoriaAcad(data) 
                                 break
                             except ValueError:
-                                print(f"{idd} es invalido")
+                                print(f"{idd} es invalido") #error en caso de que el id sea incorrecto
                     break
                 except ValueError:
-                    print (f"{selector} es invalido")
-        elif selector == 4:
+                    print (f"{selector} es invalido") #error en caso de que la seleccion sea incorrecta
+        elif selector == 4: #ejecucion en caso de que la seleccion del usuario sea 4
             while True:
                 clearConsole()
-                data = rowDeleteGetter()
+                data = rowDeleteGetter() #se ejecuta la funcion de borrado
                 deleteRow(data[0], data[1])
                 break;
         else:
