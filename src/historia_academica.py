1122e2604523e01b74678e1dd7d7d7ec4850a07e
Juan David Loaiza Reyes <joumanmaster_@outlook.es>
2022-11-15 10:11:09 -0500

    left test

--- src/historia_academica.py
+++ src/historia_academica.py
@@ -15,34 +15,74 @@ class AcadHistory(Console):
 
     def __init__(self, db):
         self.__db = db
+        self.__idMateria: int = None
+        self.__codigo: int = None
+        self.__idEstudiante: int = None
+        self.__score: float = None
+        self.__creditos: int = None
+
+    #--------------------------------------------------------------------------Setters---------------------------------------------------------------------------------
+    def __idMateriaSetter(self, idMateria: int)->None:
+        self.__idMateria = idMateria
+
+    def __codigoSetter(self, codigo: int)->None:
+        self.__codigo = codigo
+
+    def __idEstudianteSetter(self, idEstudiante: int)->None:
+        self.__idEstudiante = idEstudiante
+
+    def __scoreSetter(self, score: float)->None:
+        self.__score = score
+
+    def __creditosSetter(self, creditos: int)->None:
+        self.__creditos = creditos
+    #------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------Getters---------------------------------------------------------------------------------
+    def __idMateriaGetter(self)->int:
+        return self.__idMateria
+
+    def __codigoGetter(self)->int:
+        return self.__codigo
+
+    def __idEstudianteGetter(self)->int:
+        return self.__idEstudiante
+
+    def __scoreGetter(self)->float:
+        return self.__score
+
+    def __creditosGetter(self)->int:
+        return self.__creditos
+    #------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     #----------------------------------------------------------------Inserta las notas de la materia-------------------------------------------------------------------
 
-    def __creditsRef(self,idMateria: int):
+    def __creditsRef(self):
         try:
-            data = miMateria.searchByFilter('codigo', idMateria)
-            return data[0][5] #Retorna el codigo de la materia junto a los creditos
+            data = miMateria.searchByFilter('codigo', self.__idMateria)
+            self.__creditosSetter(data[0][5]) #Retorna el codigo de la materia junto a los creditos
         except:  
             print("Codigo de la materia inexistente") #En caso de no existir materia se ejecuta la siguiente linea
 
     #Crear historia academica en documentación
-    def __insertRow(self, codigo: int, id_estudiante: int, score: float):
+    def __insertRow(self):
         try:
             conn = sql.connect(self.__db) #Conexion a la base de datos
             conn.execute('PRAGMA foreign_keys = ON') #Valida las llaves extranjeras, es decir la existencia del estudiante o la amteria, si no existe da error
-            cursor = conn.cursor()
-            creditos= self.__creditsRef(codigo) #llamado a la funcion de filtro de materias
-            instruction = f"INSERT INTO acadhistory(code_materia, id_estudiante, nota, creditos_cursados) VALUES ({codigo},{id_estudiante}, {score}, {creditos})" #Da la instruccion de insertar datos en la base de datos
+            cursor = conn.cursor() #llamado a la funcion de filtro de materias
+            self.__creditsRef()
+            instruction = f"INSERT INTO acadhistory(code_materia, id_estudiante, nota, creditos_cursados) VALUES ({self.__codigo},{self.__idEstudiante}, {self.__score}, {self.__creditos})" #Da la instruccion de insertar datos en la base de datos
             cursor.execute(instruction)#Ejecucion de la instruccion
             conn.commit();
             conn.close(); #Cierre de la conexion a la base de datos
             print("Datos añadidos: ") #Muestra en pantalla los datos añadidos
-            miConsola.tableHistoriaAcad([(0,codigo, id_estudiante, score, creditos)])
+            miConsola.tableHistoriaAcad([(0,self.__codigo, self.__id_estudiante, self.__score, self.__creditos)])
         except sql.Error as e:
             print (e)  #En caso de error se debe verificar la existencia de los datos
             print('Probablemente no exista el estudiante o la materia, revise por favor los datos que ingreso y vuelva a intentar. Si sí existen, es que primero debe eliminar la materia, para que se actualice con los nuevos datos')
 
     #Lector del input por parte del usuario
+    #TODO add here setters
     def __rowGetter(self):
         while True:
             try:
@@ -54,16 +94,16 @@ class AcadHistory(Console):
                 ¿Desea agregar nota? 
                 1. Si. 
                 2. No.
-                """) #El usuario decide entre insertar o no insertar nota
+                """) #El usuario decide entr3e insertar o no insertar nota
                 selector = int(selector)
                 if selector ==1: #Si el usuario selecciona 1 podra insertar la nota
                     nota = input('Nota del estudiante ')
                 else: #en caso de no insertar nota aparecera como 0.0
                     nota = 0.0
-                nota = float(nota) #se transforma la nota en un dato tipo float
-                idEstudiante = int(idEstudiante) #se transforma el Id del estudiante en un dato tipo int
-                codigo = int(codigo) #se transforma el codigo en un dato tipo int
-                return (codigo, idEstudiante, nota) #retorna codigo, id y la nota
+                self.__scoreSetter(float(nota)) #se transforma la nota en un dato tipo float
+                self.__idEstudianteSetter(int(idEstudiante)) #se transforma el Id del estudiante en un dato tipo int
+                self.__codigoSetter(int(codigo)) #se transforma el codigo en un dato tipo int
+                break
             except ValueError: #En caso de error dira que los datos son invalidos
                 print("Dato(s) invalido")
 
@@ -72,11 +112,11 @@ class AcadHistory(Console):
     #----------------------------------------------------------------Lector del modulo de historia academica----------------------------------------------------------#
 
     #Esta función debe traer todos las notas de un id
-    def acadHistoryById(self, id: int):
+    def acadHistoryById(self):
         try:
             conn = sql.connect(self.__db) #Conexion a la base de datos
             cursor = conn.cursor()
-            instruction = f"SELECT * from acadhistory WHERE id_estudiante={id} ORDER BY nota DESC" #da la instruccion de buscar las notas en la base de datos
+            instruction = f"SELECT * from acadhistory WHERE id_estudiante={self.__idEstudiante} ORDER BY nota DESC" #da la instruccion de buscar las notas en la base de datos
             cursor.execute(instruction) #Ejecuta la instruccion de buscar las notas
             datos = cursor.fetchall()
             conn.commit(); 
@@ -98,23 +138,19 @@ class AcadHistory(Console):
     #-----------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
     #----------------------------------------------------------------Metodo de actualizacion--------------------------------------------------------------------------#
-    def __update(self, fieldOnChange: str, dataOnChange, code: int, dataOnChange2: int):
+    def __update(self, fieldOnChange: str):
         try:
             conn = sql.connect(self.__db) #conexion a la base de datos
             cursor = conn.cursor()
-            try:
-                dataOnChange= float(dataOnChange) #convierte la variable en un dato de tipo float
-                instruction = f"UPDATE acadhistory SET '{fieldOnChange}'={dataOnChange} WHERE code_materia={dataOnChange2} AND id_estudiante={code}" #Da la instruccion de actualizar
-            except ValueError:
-                instruction = f"UPDATE acadhistory SET '{fieldOnChange}'='{dataOnChange}' WHERE code_materia={code} AND id_estudiante={dataOnChange2}" #Comando de actualización en SQL
-            finally:
-                cursor.execute(instruction) #Ejecuta la instruccion de actualizacion
-                conn.commit();
-                conn.close() #se cierra la conexxion
-                print ('Datos actualizados de forma correcta') #Informa la correcta actualizacion de datos
+            instruction = f"UPDATE acadhistory SET '{fieldOnChange}'={self.__score} WHERE code_materia={self.__codigo} AND id_estudiante={self.__idEstudiante}" #Da la instruccion de actualizar
+            cursor.execute(instruction) #Ejecuta la instruccion de actualizacion
+            conn.commit();
+            conn.close() #se cierra la conexxion
+            print ('Datos actualizados de forma correcta') #Informa la correcta actualizacion de datos
         except sql.Error as e:
             print (e)
 
+    #TODO add setters
     def __rowUpdateGetter(self):
         while True:
             try:
@@ -123,18 +159,25 @@ class AcadHistory(Console):
                 codigo = codigo.ljust(10)#El codigo no puede tener mas de 10 caracteres
                 idEstudiante = input('Id del estudiante ')#El usuario ingresa el id
                 nota = input('Nota del estudiante ')#El usuario ingresa la nota
-                return (int(codigo), int(idEstudiante), float(nota)) #retorna los datos insertados como datos tipo int y float
+                codigo = int(codigo)
+                idEstudiante = int(idEstudiante)
+                nota = float(nota)
+                self.__codigoSetter(codigo)
+                self.__idEstudianteSetter(idEstudiante)
+                self.__scoreSetter(nota)
+                break
+                #retorna los datos insertados como datos tipo int y float
             except ValueError:
                 print("Dato(s) invalido") #En caso de que los datos sean incorrectos saldra este mensaje
 
     #-----------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
     #----------------------------------------------------------------Metodo de borrado--------------------------------------------------------------------------------#
-    def __deleteRow(self, code: int, studentId: int):
+    def __deleteRow(self):
         try:
             conn = sql.connect(self.__db) #Conexxion a la base de datos
             cursor = conn.cursor()
-            instruction = f"DELETE FROM acadhistory WHERE code_materia={code} AND id_estudiante={studentId}" #Comando de delete en SQL un solo dato
+            instruction = f"DELETE FROM acadhistory WHERE code_materia={self.__codigo} AND id_estudiante={self.__idEstudiante}" #Comando de delete en SQL un solo dato
             cursor.execute(instruction) #Ejecuta la instruccion de borrado
             conn.commit();
             conn.close() #Cierre de conexion a la base de datos
@@ -142,6 +185,7 @@ class AcadHistory(Console):
         except sql.Error as e:
             print (e)
 
+    #TODO add setter
     def __rowDeleteGetter(self):
         try:
             print("El codigo de la materia y el Id del estudiante deben existir, si no, habra error")#Aclaracion para prevenir errores en el programa
@@ -195,13 +239,13 @@ class AcadHistory(Console):
                             miMateria.readOrdered('codigo')
                             sleep(2.5)
                             print('A continuación va a inscribir una materia ')
-                            data = self.__rowGetter()
-                            self.__insertRow(data[0], data[1], data[2]) 
+                            self.__rowGetter()
+                            self.__insertRow() 
                             validator = False
                         elif selectorr == "2": #En caso de no querer ver las materias se ejecutan las siguientes lineas
                             print('A continuación va a inscribir una materia ')
-                            data = self.__rowGetter()
-                            self.__insertRow(data[0], data[1], data[2])
+                            self.__rowGetter()
+                            self.__insertRow()
                             validator = False
                     except ValueError:
                         print (f"{selector} es invalido") #Error en caso de no insertar una opcion disponible
@@ -211,7 +255,7 @@ class AcadHistory(Console):
                         miConsola.clearConsole()
                         print("Para actualizar la nota de una materia debe escribir el documento del estudiante y el codigo de la materia. Claramene la nota tambien");
                         data = self.__rowUpdateGetter() #se ejecuta la funcion de update
-                        self.__update('nota', data[2], data[1], data[0])
+                        self.__update('nota')
                         break
                     except:
                         print ("Unexpected error") #mensaje en caso de error
