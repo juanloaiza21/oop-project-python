619b05a919093582745660ed1b2d7adf320ae70e
edwin1304 <114787900+edwin1304@users.noreply.github.com>
2022-11-20 10:38:39 -0500

    Update console_utils.py

--- src/console_utils.py
+++ src/console_utils.py
@@ -13,12 +13,14 @@ class Console: #Crea la clase "Console"
 
     #Tabla para materias
     def tableMaterias(self, data): #Data va a ser una lista de tuplas de tamaño 6, coumpuesta tanto por STR como por INT
-        my_table = PrettyTable() #llamado al modulo de tablas en consola
-        my_table.field_names = ["CODIGO", "NOMBRE", "FACULTAD", "DEPARTAMENTO", "IDIOMA", "CREDITOS"] #Nombre de los campos
-        for i in range(len(data)):
-            my_table.add_row(data[i])
-        print(my_table)
-
+        if (len(data))!=0:
+            my_table = PrettyTable() #llamado al modulo de tablas en consola
+            my_table.field_names = ["CODIGO", "NOMBRE", "FACULTAD", "DEPARTAMENTO", "IDIOMA", "CREDITOS"] #Nombre de los campos
+            for i in range(len(data)):
+                my_table.add_row(data[i])
+            print(my_table)
+        elif (len(data))==0:
+            print("¡¡dato invalido!!")
     #Tabla de historia academica
     def tableHistoriaAcad(self, data): #Data va a ser una lista de tuplas de tamaño = 5, compuesta en su totalidad por INT
         my_table = PrettyTable() #llamado al modulo de tablas en consola
@@ -29,12 +31,14 @@ class Console: #Crea la clase "Console"
 
     #Tabla Estudiante
     def tableEstudiante(self, data): #Data va a ser una lista de tuplas. con tamaño = 9, compuesto por INT y STRINGS, tal como estan en la BD
-        my_table = PrettyTable() #llamado al modulo de tablas en consola
-        my_table.field_names = ["IDENTIFICACION", "NOMBRE", "APELLIDO", "CARRERA", "FECHA NACIMIENTO", "FECHA INGRESO", "PROCEDENCIA", "CORREO ELECTRONICO", "CANTIDAD DE MATRICULAS"] #Nombre de los campos
-        for i in range(len(data)):
-            my_table.add_row(data[i])
-        print(my_table)
-
+        if (len(data))!=0:
+            my_table = PrettyTable() #llamado al modulo de tablas en consola
+            my_table.field_names = ["IDENTIFICACION", "NOMBRE", "APELLIDO", "CARRERA", "FECHA NACIMIENTO", "FECHA INGRESO", "PROCEDENCIA", "CORREO ELECTRONICO", "CANTIDAD DE MATRICULAS"] #Nombre de los campos
+            for i in range(len(data)):
+                my_table.add_row(data[i])
+            print(my_table)
+        elif (len(data))==0:
+            print("¡¡dato invalido!!")
 
     #Tabla de historia academica
     def tableRanking(self, data): #Data va a ser una lista de tuplas, cada tupla será de un tamaño = 2, ambos datos seran int
