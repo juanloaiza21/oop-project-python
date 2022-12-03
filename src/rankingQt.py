c9781d53c82ec576b3b06e51e0476c4460d7df98
Juan David Loaiza Reyes <joumanmaster_@outlook.es>
2022-12-03 12:06:21 -0500

    refactor

--- src/rankingQt.py
+++ src/rankingQt.py
@@ -1,59 +1,61 @@
-from PyQt5.QtWidgets import *
-import sys
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtCore import Qt
 from ranking import Ranking
-
-
-
-class TableModel(QtCore.QAbstractTableModel):
-    def __init__(self, data):
-        super(TableModel, self).__init__()
-        self._data = data
-    
-    def headerData(self, p_int, Qt_Orientation, role=None):
-        if role == Qt.DisplayRole and Qt_Orientation==Qt.Horizontal:
-            header = [ "ID", "NOMBRE", "APELLIDO", "PROMEDIO", "CREDITOS", "MATERIAS"]
-            return header[p_int]
-        else:
-            return QtCore.QAbstractTableModel.headerData(self, p_int, Qt_Orientation, role)        
-
-    def data(self, index, role):
-        if role == Qt.DisplayRole:
-            # See below for the nested-list data structure.
-            # .row() indexes into the outer list,
-            # .column() indexes into the sub-list
-            return self._data[index.row()][index.column()]
-
-    def rowCount(self, index):
-        # The length of the outer list.
-        return len(self._data)
-
-    def columnCount(self, index):
-        # The following takes the first sub-list, and returns
-        # the length (only works if all rows are an equal length)
-        return len(self._data[0])
-
-
-
-
-
-class MainWindow(QtWidgets.QMainWindow):
+import sys
+from PyQt5.uic import loadUi
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import * 
+                    
+   
+#Main Window
+class App(QWidget):
     def __init__(self):
         super().__init__()
-        self._ranking = Ranking("DBTEST.db")
-        self.table = QtWidgets.QTableView()
-        self.data = self._ranking.dataUnprint()
-        self.setFixedWidth(620)
-        self.setFixedHeight(700)
-        self.setWindowTitle("Ranking de mayor promedio a menor")
-        self.model = TableModel(self.data)
-        self.table.setModel(self.model)
-        self.setCentralWidget(self.table)
-        
+        self.title = 'PyQt5 - QTableWidget'
+        self.left = 0
+        self.top = 0
+        self.width = 300
+        self.height = 200
+   
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+   
+        self.createTable()
+   
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.tableWidget)
+        self.setLayout(self.layout)
+   
+        #Show window
+        self.show()
+   
+    #Create table
+    def createTable(self):
+        rank = Ranking("DBTEST.db")
+        data = rank.dataUnprint()
 
+        self.tableWidget = QTableWidget()
+        self.title = "POO FINAL G1-G1"
+        self.tableWidget.setColumnCount(6)  
+        self.tableWidget.setRowCount(len(data))
 
-app=QtWidgets.QApplication(sys.argv)
-window=MainWindow()
-window.show()
-app.exec_()
+        self.tableWidget.setHorizontalHeaderLabels(["Codigo","Nombre", "Apellido", "Promedio", "Materias", "Creditos"]) 
+
+        #Imprimir datos
+        for i in range(len(data)):
+            element = data[i]
+            self.tableWidget.setItem(i,0, QTableWidgetItem(str(element[0])))
+            self.tableWidget.setItem(i,1, QTableWidgetItem(str(element[1])))
+            self.tableWidget.setItem(i,2, QTableWidgetItem(str(element[2])))
+            self.tableWidget.setItem(i,3, QTableWidgetItem(str(element[3])))
+            self.tableWidget.setItem(i,4, QTableWidgetItem(str(element[4])))
+            self.tableWidget.setItem(i,5, QTableWidgetItem(str(element[5])))            
+
+    
+        #Table will fit the screen horizontally
+        self.tableWidget.horizontalHeader().setStretchLastSection(True)
+        self.tableWidget.horizontalHeader().setSectionResizeMode(
+            QHeaderView.Stretch)
+   
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
